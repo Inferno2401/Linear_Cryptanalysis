9017f8724d6e3a732fee314a6d82e6e48bf111ff
mathismusic <krishna.agaram1729@gmail.com>
2023-04-19 19:56:01 +0530

    updated files

--- des.py
+++ des.py
@@ -1,20 +1,6 @@
 import numpy as np
 from constants import *
 
-def convert_to_bitstring(arr: np.ndarray):
-    n = 8
-    m = 64
-    ans = [[""]*m for _ in range(n)]
-    print(bin(3)[2:].zfill(4))
-    for i in range(n):
-        for j in range(m):
-            print(bin(arr[i][j])[2:].zfill(4))
-            ans[i][j] = bin(arr[i][j])[2:].zfill(4)
-            print(ans[i][j])
-    import json
-    with open('sub_boxes.txt', 'w') as f:
-        f.write(json.dumps(ans))
-
 # convert_to_bitstring(substitution_boxes)
 def expand(half_block: str):
     return np.array(list(half_block[expansion_table[i]] for i in range(48)))
@@ -30,12 +16,12 @@ def feistel_fn(half_block: str, subkey: str):
     expanded = expand(half_block)
 
     # key mixing
-    result = xor(expanded, subkey); print(result)
+    result = xor(expanded, subkey); #print(result)
 
     # apply the S-boxes.
     # print(list(int(result[i] + result[i+5] + result[i+1:i+5], base=2) for i in range(0, 48, 6)))
     res = "".join(substitution_boxes[i//6][int(result[i] + result[i+5] + result[i+1:i+5], base=2)] for i in range(0, 48, 6))
-    print(res)
+    #print(res)
     # permute the result
     
     final_ans = "".join(res[feistel_permutation[i]] for i in range(32))
@@ -77,18 +63,22 @@ def des(block: str, key: str):
     ciphertext = "".join(block[elem] for elem in inv_permutation1)
     return ciphertext
 
-def encrypt(plaintext: str, key: str):
+def encrypt(plaintext: str, key: str, iv: str='0'*64):
+    """
+    Encrypting arbitrary length messages using 64-bit blocks
+    """
     floor = (len(plaintext)>>6)<<6
     if len(plaintext) != floor:
         plaintext = plaintext + '1' + '0'*(floor + 63 - len(plaintext))
         floor += 64
     return "".join(des(block=plaintext[i:i+64], key=key) for i in range(0, floor, 64))
 
+# simple driver code
 if __name__ == "__main__":
     plaintext = "1010101111001101111001101010101111001101000100110010010100110110"
     key =       "1010101010111011000010010001100000100111001101101100110011011101"
 
-    cipher = des(plaintext, key)
+    cipher = encrypt(plaintext, key)
     print(cipher)
     expected_output = "1001111000100110100111110101101011111010010011011011101101110000"
     print(cipher == expected_output)
