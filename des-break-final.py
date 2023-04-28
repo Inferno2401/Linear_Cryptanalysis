eb8bd9b305eff6660cb042bd9931a702a0c34581
mathismusic <krishna.agaram1729@gmail.com>
2023-04-28 18:00:44 +0530

    updated files

--- des-break-final.py
+++ des-break-final.py
@@ -14,11 +14,11 @@ def multi_xor(string: str, indices: list):
 ROUNDS = 1
 p0 = 0.19
 N = 2000 # for probability of success 99.8%
-
+good = []; bad = []
 
 def test(KEY):
     KEY_56BIT = "".join(KEY[elem] for elem in des_constants.pc1)[:56]
-    # print(KEY_56BIT)
+    print(KEY_56BIT[:5])
     random_ptexts = zip(np.random.randint(1<<32, size=(N,)), np.random.randint(1<<32, size=(N,)))
     P = list(map(lambda x: (bin(x[0])[2:].zfill(32) + bin(x[1])[2:].zfill(32)), random_ptexts))
     C = list(des.encrypt(p, KEY, ROUNDS) for p in P)
@@ -31,16 +31,24 @@ def test(KEY):
         prob += (multi_xor(p, [2, 7, 13, 24, 48])^multi_xor(c, [2, 7, 13, 24]) == multi_xor(KEY_56BIT, [3]))
         T += (multi_xor(p, [2, 7, 13, 24, 48])^multi_xor(c, [2, 7, 13, 24]))
     # print(prob/N)
-    return ((p0<0.5) if T<N/2 else (p0>0.5)), prob
+    if prob < 0.5: good.append(KEY_56BIT[:5])
+    else: bad.append(KEY_56BIT[:5])
+    return int((p0<0.5) if T<N/2 else (p0>0.5)), prob
 
 def solve(KEY):
-    print(KEY)
+    # print(KEY)
     while True:
         ans, prob = test(KEY)
-        if abs(p0 - prob) < 0.01: return ans
+        print(prob/N)
+        # if abs(p0 - prob) < 0.01: 
+        return ans
 
-KEY = bin(np.random.randint(1<<32))[2:].zfill(32) + bin(np.random.randint(1<<32))[2:].zfill(32)
-KEY_56BIT = "".join(KEY[elem] for elem in des_constants.pc1)[:56]
-ans = solve(KEY)
-print("K[3]:", multi_xor(KEY_56BIT, [3]))
-print("Ours:", ans)
+for i in range(10):
+    KEY = bin(np.random.randint(1<<32))[2:].zfill(32) + bin(np.random.randint(1<<32))[2:].zfill(32)
+    KEY_56BIT = "".join(KEY[elem] for elem in des_constants.pc1)[:56]
+    ans = solve(KEY)
+    print("K[3]:", multi_xor(KEY_56BIT, [3]))
+    print("Ours:", ans)
+    print()
+print(good)
+print(bad)
